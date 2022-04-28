48b66742334dec1153d17f63c8a1e0690bf48738
Alan-aj <alanj14333@gmail.com>
2022-04-28 16:44:22 +0530

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("https://chat-joy.herokuapp.com");
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState("");
